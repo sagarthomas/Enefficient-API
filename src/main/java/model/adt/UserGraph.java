4064345960dcb01dcf0c6d6e8272e898677219ee
Matthew Dombrady <dombradm@mcmaster.ca>
2018-03-28 21:26:05 -0400

    Hi

---
+++ src/main/java/model/adt/UserGraph.java
@@ -0,0 +1,37 @@
+package model.adt;
+
+import java.util.ArrayList;
+
+import model.util.Bag;
+
+public class UserGraph {
+	
+	private final int V;
+	private int E = 0;
+	private Bag<UserADT>[] adj;
+	
+	public UserGraph(ArrayList<UserADT> users, int V) {
+		this.V = V;
+		
+		for (int v = 0; v < V; v++)
+			adj[v] = new Bag<UserADT>();
+		
+		for (int i = 0; i < V; i++) {
+			for (int j = 0; j < users.size(); j++) {
+				if (users.get(j).getHouseType() == "") {
+					adj[i].add(users.get(j));
+				}
+			}
+		}
+		
+	}
+	
+	public int V() {
+		return this.V;
+	}
+	
+	public Bag<UserADT> adj(int v) {
+		return this.adj[v];
+	}
+
+}
